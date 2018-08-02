3e2619e7b4ad2369a658b6dbde1820c5f9d9fcc1
Sygix <admin@sygix.fr>
2018-08-02 20:28:09 +0200

    Temp channels auto setup finished

--- src/commands/setupvoice.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'setupvoice',
-    description: 'Configuration du serveur pour accepter les channels temporaire.',
-    aliases: ['voicesetup'],
-    cooldown: 10,
-    execute(msg, args) {
-        msg.channel.send("Commande en cours de codage...");
-    },
-};
