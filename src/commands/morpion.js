525d1edd99e073fd4e9cb7344498cadd2c4ed15d
Sygix <admin@sygix.fr>
2018-08-03 21:44:24 +0200

    Fix multiplayers glitch in Morpion/TicTacToe Voice channel category bug has been fixed in the previous commit

--- src/commands/morpion.js
+++ src/commands/morpion.js
@@ -11,21 +11,18 @@ module.exports = {
             var morpion = new morpionGame(msg, msg.member);
             morpion.start(['human', 'ai']);
         }else{
-            var player1 = msg.member;
-            var player2 = msg.mentions.members.array()[0];
-            if(player2 === player1)return;
-
-            var morpion = new morpionGame(msg, player1, player2);
-            morpion.start(['human', 'human']);
-            /*if(msg.mentions.members.array().length <= 0)return;
-            if(msg.guild.members.get(msg.mentions.members.array()[0].id)){
+            if(msg.mentions.members.array().length <= 1)return;
+            if(msg.guild.members.has(msg.mentions.members.array()[1].id)){
                 var player1 = msg.member;
                 var player2 = msg.mentions.members.array()[0];
                 if(player2 === player1)return;
+                if(player2.user === bot.user) return;
+                if(player2.user.bot) return;
+
 
                 var morpion = new morpionGame(msg, player1, player2);
                 morpion.start(['human', 'human']);
-            }*/
+            }
         }
     },
 };
