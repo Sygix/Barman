0b1b2aa9b6e87d27e746c717250968a764b47ff1
Sygix <admin@sygix.fr>
2019-12-17 18:58:56 +0100

    Testing music system on development ¯\_(ツ)_/¯

--- src/commands/music.js
+++ src/commands/music.js
@@ -6,23 +6,38 @@ module.exports = {
     aliases: ['musique', 'm'],
     exemple: '(play, stop)',
     cooldown: 0,
-    args: false,
+    args: true,
     guildOnly: true,
     hidden: true,
     category: 'music',
     active: true,
-    execute(msg, args) {
-        const channel = bot.channels.get("291817675540135936");
-        if (!channel) return console.error("The channel does not exist!");
-        channel.join().then(connection => {
-            const stream = ytdl('https://www.youtube.com/watch?v=ZkqyIoYAXV8', { filter : 'audioonly' });
-            const dispatcher = connection.playStream(stream, { seek: 0, volume: 1 });
-            dispatcher.on("end", end => {
-                channel.leave();
+    async execute(msg, args) {
+
+        if(!msg.member.voiceChannel)
+            return msg.channel.send("You must be in a voice channel for me to start playing music");
+        if(msg.guild.me.voiceChannel)
+            return msg.channel.send("Im busy at a different voice channel on the server. Try again later.");
+
+        if(!args[0])
+            return msg.channel.send("Please enter a youtube URL for me to load the song from.");
+
+        let validate = await ytdl.validateURL(args[0]);
+        if(!validate)
+            return msg.channel.send("Whoops, re-check the URL you gave me, I am getting an error while trying to play the song. ");
+
+        let info = await ytdl.getInfo(args[0]);
+        let voiceChannel = msg.member.voiceChannel;
+        let connection = await voiceChannel.join();
+        let dispatcher = await connection.playStream(ytdl(args[0], {filter: 'audioonly'}))
+            .on("end", end => {
+                msg.channel.send(`Finished Playing: ${info.title}`);
+                voiceChannel.leave();
+            })
+            .on("error", error => {
+                console.error(error);
+                msg.channel.send("Error Occurred during playback. Try again later.");
             });
-        }).catch(e => {
-            console.error(e);
-        });
+        return msg.channel.send(`Now Playing: ${info.title}`);
     },
 };
 
