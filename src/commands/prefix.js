56ed8b340add1b8e31919ab2b0eaabbab69f0a65
Sygix <admin@sygix.fr>
2019-12-17 17:50:09 +0100

    Fix prefix rights + Testing on development ¯\_(ツ)_/¯

--- src/commands/prefix.js
+++ src/commands/prefix.js
@@ -13,10 +13,14 @@ module.exports = {
     active: true,
     execute(msg, args) {
         if(typeof args[0] !== "undefined" && args[0].toLowerCase() === 'set'){
-            firebase.updatePrefix(msg.guild.id, args[1]);
-            firebase.getServerSettings(msg.guild.id, (snap) => {
-                msg.channel.send("Nouveau préfix défini sur '" + snap.val().prefix + "'");
-            });
+            if(msg.channel.permissionsFor(msg.author).has('MANAGE_GUILD')){
+                firebase.updatePrefix(msg.guild.id, args[1]);
+                firebase.getServerSettings(msg.guild.id, (snap) => {
+                    msg.channel.send("Nouveau préfix défini sur '" + snap.val().prefix + "'");
+                });
+            }else{
+                msg.reply("Vous n'avez pas les droits nécéssaire au changement du préfix. ('MANAGE_GUILD')");
+            }
         }else{
             firebase.getServerSettings(msg.guild.id, (snap) => {
                 msg.channel.send("Le préfix actuel du serveur est '" + snap.val().prefix + "'");
