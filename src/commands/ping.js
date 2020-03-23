1fee2de40ea49a5987cd80a39698aa46ef499563
Sygix <admin@sygix.fr>
2020-03-23 18:54:18 +0100

    Ping command is now giving real latency

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -3,7 +3,11 @@ module.exports = {
     description: 'Ping!',
     active: true,
     cooldown: 10,
-    execute(msg, args) {
-        msg.channel.send('pong :ping_pong:');
+    async execute(msg, args) {
+        const message = await msg.channel.send(`:ping_pong: Pinging....`);
+
+        message.edit(`:ping_pong: Pong!
+        La latence est de ${Math.floor(message.createdTimestamp - msg.createdTimestamp)}ms
+        La latence API est de ${Math.round(bot.ping)}ms`);
     },
 };
